965c525f6ec08aed18213d399000b2fcf89178e7
Your Name <qwert6555013@gmail.com>
2026-06-26 16:00:22 +0530

    Removed Duplicate Files

--- client/src/net/socket.js
+++
@@ -1,6 +0,0 @@
-import { io } from "socket.io-client";
-const serverUrl = import.meta.env.VITE_SERVER_URL ?? "https://couple-tie.onrender.com";
-export const socket = io(serverUrl, {
-    transports: ["websocket", "polling"],
-    autoConnect: true,
-});
